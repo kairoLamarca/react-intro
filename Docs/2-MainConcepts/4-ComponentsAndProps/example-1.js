de8aa4c37fa2c3c00b8a2c4f5cf98ba01cd6cdc4
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-07-25 21:40:52 -0300

    Exemplos de props
    Exemplos de props

---
+++ Docs/2-MainConcepts/4-ComponentsAndProps/example-1.js
@@ -0,0 +1,11 @@
+//Antigo
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+//ES6
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}

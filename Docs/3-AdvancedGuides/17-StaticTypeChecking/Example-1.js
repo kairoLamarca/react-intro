921ed22f16bf5aa2bcb519407e4bdcaa4444daae
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-08-30 21:57:53 -0300

    Exemplos de utilização do Flow
    Exemplos de utilização do Flow

---
+++ Docs/3-AdvancedGuides/17-StaticTypeChecking/Example-1.js
@@ -0,0 +1,57 @@
+//Se você usar o Yarn , execute:
+//yarn add --dev flow-bin
+
+//Se você usar o npm , execute:
+//npm install --save-dev flow-bin
+
+//scripts
+// {
+//     // ...
+//     "scripts": {
+//       "flow": "flow",
+//       // ...
+//     },
+//     // ...
+//   }
+
+//-----
+// Se você usar o Yarn , execute:
+// yarn run flow init
+
+// Se você usar o npm , execute:
+// npm run flow init
+
+//--------------------
+//Se for Babel
+
+// Se você usar o Yarn, execute:
+// yarn add --dev babel-preset-flow
+
+// Se você usar o npm, execute:
+// npm install --save-dev babel-preset-flow
+
+// {
+//     "presets": [
+//       "flow",
+//       "react"
+//     ]
+//   }
+
+//----------------------------------------------------
+
+// Flow de execução
+// Se você seguiu as instruções acima, poderá executar o Flow pela primeira vez.
+
+// yarn flow
+// Se você usar o npm, execute:
+ 
+// npm run flow
+// Você deve ver uma mensagem como:
+
+// No errors!
+// ✨  Done in 0.17s.
+
+//--------------------------------------------------------
+
+//Por padrão, o Flow verifica apenas os arquivos que incluem essa anotação:
+// @flow

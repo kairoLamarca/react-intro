c89ec4ec6c12339bcf6da3f2ad06c6369f9fa4e3
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-09-11 22:00:26 -0300

    mostrando o X ao clicar no quadrado
    mostrando o X ao clicar no quadrado

--- Tutorial/tic-tac-toe/src/App.js
+++ Tutorial/tic-tac-toe/src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        {this.state.value}
       </button>
     );
   }
